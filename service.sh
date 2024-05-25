9bb467c9d2f7b132f084c2ef43047076143aade0
雪月三十 <61481786+XueyueThirty@users.noreply.github.com>
2024-05-25 22:20:28 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -4,7 +4,7 @@ sed -i 's/POWER_SUPPLY_CHARGE_FULL=.*./POWER_SUPPLY_CHARGE_FULL=4000000/' /sys/c
 sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=4000000/' /sys/class/power_supply/bms/uevent
 sed -i 's/POWER_SUPPLY_CHARGE_FULL=.*./POWER_SUPPLY_CHARGE_FULL=4000000/' /sys/class/power_supply/battery/uevent
 sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=4000000/' /sys/class/power_supply/battery/uevent
-echo 4000000 > /sys/class/power_supply/bms/charge_full
-echo 4000000 > /sys/class/power_supply/bms/charge_full_design
-echo 4000000 > /sys/class/power_supply/battery/charge_full
-echo 4000000 > /sys/class/power_supply/battery/charge_full_design
+echo 5000000 > /sys/class/power_supply/bms/charge_full
+echo 5000000 > /sys/class/power_supply/bms/charge_full_design
+echo 5000000 > /sys/class/power_supply/battery/charge_full
+echo 5000000 > /sys/class/power_supply/battery/charge_full_design
